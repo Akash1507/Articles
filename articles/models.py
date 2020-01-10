f99036fe2c26a0dd877d4cabc37e8e72b52b996a
Akash1507 <atakashsharma@gmail.com>
2020-01-10 19:04:48 +0530

    project implementing rest framework

---
+++ articles/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.title
