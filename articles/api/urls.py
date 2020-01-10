7e03d2c68dbfae39e04c4b06cec83ebba1cb43a3
Akash1507 <atakashsharma@gmail.com>
2020-01-11 00:36:40 +0530

    add router for url patterns

--- articles/api/urls.py
+++ articles/api/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path
+from articles.api.views import ArticleViewset
+from rest_framework.routers import DefaultRouter
+# from articles.api.views import ArticleListView, ArticleDetailView,ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
-from articles.api.views import ArticleListView, ArticleDetailView,ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
-urlpatterns = [
-    path('',ArticleListView.as_view()),
-    path('<pk>/',ArticleDetailView.as_view()),
-    path('create/',ArticleCreateView.as_view()),
-    path('<pk>/update',ArticleUpdateView.as_view()),
-    path('<pk>/delete',ArticleDeleteView.as_view())
-]
+router = DefaultRouter()
+router.register(r'',ArticleViewset,basename='articles')
+urlpatterns = router.urls
+
+
+
+
+
+
+# urlpatterns = [
+#     path('',ArticleListView.as_view()),
+#     path('<pk>/',ArticleDetailView.as_view()),
+#     path('create/',ArticleCreateView.as_view()),
+#     path('<pk>/update',ArticleUpdateView.as_view()),
+#     path('<pk>/delete',ArticleDeleteView.as_view())
+# ]
